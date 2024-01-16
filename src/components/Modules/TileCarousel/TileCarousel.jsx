65ee0d6bd9e918a91e0301e240b1bf0e6e7d4656
Paul <paul@yourcreative.com.au>
2024-01-16 11:18:28 +1100

    .

--- src/components/Modules/TileCarousel/TileCarousel.jsx
+++ src/components/Modules/TileCarousel/TileCarousel.jsx
@@ -1,3 +1,7 @@
+import cn from 'clsx'
+
+import ImageBox from '@/components/ImageBox/ImageBox'
+
 import s from './TileCarousel.module.sass'
 
 export default function TileCarousel ({
@@ -5,11 +9,37 @@ export default function TileCarousel ({
     content,
     tiles
 }) {
+    console.log(title, content, tiles)
+
     return (
-        <section className={s.Outer}>
-            <h1>
-                Tile carousel
-            </h1>
+        <section className={s.TileCarousel}>
+            <div className={cn(s.Cont, "Container")}>
+                <ul>
+                    {tiles?.map((tile, index) => {
+                        const {
+                            preTitle,
+                            title,
+                            copy,
+                            image,
+                        } = tile
+
+                        return (
+                            <li key={index}>
+                                { preTitle &&
+                                    <div className="smallTitle">{preTitle}</div>
+                                }
+                                <h2>{title}</h2>
+                                <p>{copy}</p>
+                                { image &&
+                                    <div className={s.Image}>
+                                        <ImageBox image={image} />
+                                    </div>
+                                }
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </section>
     )
 }
