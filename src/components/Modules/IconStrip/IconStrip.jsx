a579f13ade2e71a8cace9b33a5c60760046cfb76
Paul <paul@yourcreative.com.au>
2024-01-16 11:09:52 +1100

    IconStrip

--- src/components/Modules/IconStrip/IconStrip.jsx
+++ src/components/Modules/IconStrip/IconStrip.jsx
@@ -1,3 +1,7 @@
+import cn from 'clsx'
+
+import ImageBox from '@/components/ImageBox/ImageBox'
+
 import s from './IconStrip.module.sass'
 
 export default function IconStrip ({
@@ -5,10 +9,25 @@ export default function IconStrip ({
     icons
 }) {
     return (
-        <section className={s.Outer}>
-            <h1>
-                Icon strip
-            </h1>
+        <section className={s.IconStrip}>
+            <div className={cn(s.Cont, "Container")}>
+                { title &&
+                    <div className="smallTitle">{title}</div>
+                }
+                {icons &&
+                    <ul>
+                        {icons.map((icon, index) => {
+                            if (!icon) return null
+
+                            return (
+                                <li key={index}>
+                                    <ImageBox image={icon} />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                }
+            </div>
         </section>
     )
 }
