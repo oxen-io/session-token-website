e65ee0ab2bdc5f6cfcf8b9727b7698dea90d37ca
William Grant <willmgrant@gmail.com>
2024-05-28 15:59:47 +1000

    fix: separate layouts for website route and root of the entire app
    this prevents website content being injected into sanity studio

---
+++ app/(Site)/layout.tsx
@@ -0,0 +1,48 @@
+import { SettingsProvider } from '@/components/Contexts/SettingsContext';
+import { DevModal } from '@/components/DevModal';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+import { PreviewBanner } from '@/components/preview/PreviewBanner';
+import PreviewProvider from '@/components/preview/PreviewProvider';
+import { isNotProduction } from '@/lib/env';
+import { getSettings, token } from '@/lib/sanity.fetch';
+import '@/styles/Global.sass';
+import '@/styles/Reset.sass';
+import { AtypDisplay } from '@/styles/fonts/fonts';
+import '@/styles/globals.css';
+import clsx from 'clsx';
+import { draftMode } from 'next/headers';
+import type { ReactNode } from 'react';
+
+const OptionalPreviewProvider = ({
+  children,
+  isDraftMode,
+}: {
+  children: ReactNode;
+  isDraftMode: boolean;
+}) => {
+  return isDraftMode ? (
+    <PreviewProvider token={token}>{children}</PreviewProvider>
+  ) : (
+    <>{children}</>
+  );
+};
+
+export default async function SiteLayout({ children }: { children: React.ReactNode }) {
+  const isDraftMode = draftMode().isEnabled;
+  const settings = await getSettings();
+
+  return (
+    <div className={clsx(AtypDisplay.className, 'text-text')}>
+      <OptionalPreviewProvider isDraftMode={isDraftMode}>
+        {isDraftMode ? <PreviewBanner /> : null}
+        {isNotProduction() ? <DevModal /> : null}
+        <SettingsProvider value={settings}>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </SettingsProvider>
+      </OptionalPreviewProvider>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,9 @@
-import { SettingsProvider } from '@/components/Contexts/SettingsContext';
-import { DevModal } from '@/components/DevModal';
-import { Footer } from '@/components/Footer';
-import { Header } from '@/components/Header';
-import { PreviewBanner } from '@/components/preview/PreviewBanner';
-import PreviewProvider from '@/components/preview/PreviewProvider';
-import { isNotProduction } from '@/lib/env';
-import { getSettings, token } from '@/lib/sanity.fetch';
-import '@/styles/Global.sass';
-import '@/styles/Reset.sass';
-import { AtypDisplay } from '@/styles/fonts/fonts';
-import '@/styles/globals.css';
-import { draftMode } from 'next/headers';
 import type { ReactNode } from 'react';
 
-const OptionalPreviewProvider = ({
-  children,
-  isDraftMode,
-}: {
-  children: ReactNode;
-  isDraftMode: boolean;
-}) => {
-  return isDraftMode ? (
-    <PreviewProvider token={token}>{children}</PreviewProvider>
-  ) : (
-    <>{children}</>
-  );
-};
-
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const isDraftMode = draftMode().isEnabled;
-  const settings = await getSettings();
-
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${AtypDisplay.className}`}>
-      <body className="text-text">
-        <OptionalPreviewProvider isDraftMode={isDraftMode}>
-          {isNotProduction() ? <DevModal /> : null}
-          {isDraftMode ? <PreviewBanner /> : null}
-          <SettingsProvider value={settings}>
-            <Header />
-            <main>{children}</main>
-            <Footer />
-          </SettingsProvider>
-        </OptionalPreviewProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
