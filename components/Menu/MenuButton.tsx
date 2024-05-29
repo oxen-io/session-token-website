7a4e1be4b053fc4749761f265a6e490a8a1c94d2
William Grant <willmgrant@gmail.com>
2024-05-29 14:23:18 +1000

    feat: moved some of the menu button styles to tailwind

--- components/Menu/MenuButton.tsx
+++ components/Menu/MenuButton.tsx
@@ -1,9 +1,25 @@
 import clsx from 'clsx';
 import s from './MenuButton.module.sass';
 
-export default function MenuButton({ open, setOpen }: { open: boolean; setOpen: () => void }) {
+export default function MenuButton({
+  open,
+  setOpen,
+  className,
+}: {
+  open: boolean;
+  setOpen: () => void;
+  className?: string;
+}) {
   return (
-    <button className={clsx(s.Outer, open && s.Open)} onClick={setOpen}>
+    <button
+      className={clsx(
+        'h-9 w-9 border border-solid border-primary rounded-full ml-1',
+        s.Outer,
+        open && s.Open,
+        className
+      )}
+      onClick={setOpen}
+    >
       <div />
       <div />
       <div />
