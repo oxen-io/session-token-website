0438e96495c6601f8f0118351295c078779561b5
William Grant <willmgrant@gmail.com>
2024-05-29 15:24:39 +1000

    feat: finished desktop style
    only render certain buttons depending on the screen width

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,6 +9,7 @@ import Button from '@/components/Button/Button';
 import { SettingsContext } from '@/components/Contexts/SettingsContext';
 import NavLink from '@/components/NavLink/NavLink';
 import Socials from '@/components/Socials/Socials';
+import { useScreenWidth } from '@/hooks/screen';
 import Logo from '@/public/images/logo.png';
 import MenuButton from '../Menu/MenuButton';
 
@@ -18,56 +19,63 @@ export function Header() {
     setIsExpanded(!isExpanded);
   };
 
+  const { isSM, isMD } = useScreenWidth();
+
   const { menuTopLink, mobileMenuCta, menuItems } = useContext(SettingsContext);
 
   return (
     <nav
       role="navigation"
+      // Try to match the global Container class
       className={clsx(
-        'relative flex items-center justify-between z-10 w-full',
+        'relative flex items-center justify-between z-10 w-full max-w-[1580px] pt-7',
+        'lg:pt-5 lg:px-10 lg:mx-auto',
         'transition duration-300'
       )}
     >
-      <div
-        className={clsx('flex justify-between items-center w-full px-5 pt-7', 'lg:w-1/3 lg:p-0')}
-      >
+      <div className={clsx('flex justify-between items-center w-full px-5', 'lg:w-fit lg:px-0')}>
         <NavLink href="/">
           <Image
             src={Logo}
             alt="Session Token"
-            className={clsx('w-36', 'lg:w-auto lg:h-[29px]')}
+            className={clsx('w-36', 'lg:w-48 lg:h-auto')}
             priority
           />
         </NavLink>
-        <div className={clsx('flex flex-row')}>
-          <Button {...menuTopLink} small />
-          <MenuButton open={isExpanded} setOpen={toggleNav} />
-        </div>
+        {isSM || isMD ? (
+          <div className={clsx('flex flex-row')}>
+            <Button {...menuTopLink} small />
+            <MenuButton open={isExpanded} setOpen={toggleNav} />
+          </div>
+        ) : null}
       </div>
       <div
         className={clsx(
           'absolute top-20 left-0 right-0 w-full overflow-hidden z-0',
-          'lg:relative lg:overflow-visible lg:w-2/3 lg:top-0',
-          'transform transition-all duration-300'
+          'lg:relative lg:overflow-visible lg:top-0'
         )}
       >
         <div
           className={clsx(
             'bg-background flex flex-col items-start justify-between text-lg text-white w-full px-5',
-            'lg:text-base lg:flex-row lg:items-center lg:justify-end lg:font-bold lg:text-gray',
+            'lg:bg-none lg:flex-row lg:items-center lg:px-0',
             'transform transition-all duration-300',
             isExpanded
               ? 'h-screen translate-y-0 -mb-20 pt-3 pb-10'
-              : 'h-0 -translate-y-full lg:translate-y-0'
+              : 'h-0 -translate-y-full lg:h-auto lg:translate-y-0'
           )}
         >
           <div
-            className={clsx('flex flex-col justify-start items-start w-full max-w-[1000px] gap-5')}
+            className={clsx(
+              'flex flex-col justify-start items-start w-full gap-5',
+              'lg:flex-row lg:gap-12 lg:ml-12'
+            )}
           >
             {menuItems.map((item, index) => {
               return (
                 <div
                   key={`${item.title}-${index}`}
+                  onClick={isSM || isMD ? toggleNav : undefined}
                   className={clsx('cursor-pointer', 'hover:text-primary')}
                 >
                   <NavLink
@@ -80,10 +88,15 @@ export function Header() {
               );
             })}
           </div>
-          <div className={clsx('flex flex-col pb-16')}>
-            <Button {...mobileMenuCta} />
-            <Socials />
-          </div>
+
+          {isSM || isMD ? (
+            <div className={clsx('flex flex-col pb-16')}>
+              <Button {...mobileMenuCta} />
+              <Socials />
+            </div>
+          ) : (
+            <Button {...menuTopLink} className={'h-9 text-lg'} />
+          )}
         </div>
       </div>
     </nav>
