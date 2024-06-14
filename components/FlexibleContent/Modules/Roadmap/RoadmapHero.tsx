422d5a760cc4eafb77bc199295dd513a24ce215d
Ryan Miller <ryan.m@getsession.org>
2024-06-14 18:15:36 +1000

    fix: build properly responsive roadmap page components

--- components/FlexibleContent/Modules/Roadmap/RoadmapHero.tsx
+++ components/FlexibleContent/Modules/Roadmap/RoadmapHero.tsx
@@ -11,7 +11,6 @@ import {
 
 import clsx from 'clsx';
 
-import ScrollButton from '@/components/Button/ScrollButton';
 import type React from 'react';
 import s from './RoadmapHero.module.sass';
 
@@ -35,44 +34,41 @@ export default function RoadmapHero({
   children: React.ReactNode;
 }) {
   return (
-    <section className={clsx(s.Hero)}>
-      <div className={clsx(s.Cont, 'Container', 'w-[1500px]')}>
-        <ScrollButton />
-        <div className={clsx(s.CopyCont)}>
-          {subtitle && (
-            <AnimatedElement
-              type="h3"
-              delay={0}
-              dangerouslySetInnerHTML={{ __html: subtitle }}
-              className={clsx(containsNewLine(title) && '-mt-24')}
-            />
-          )}
-          {title && (
-            <AnimatedElement
-              type="h1"
-              className="mt-8 bg-gradient-to-tr from-[#FFFFFF] to-[#97A99E] bg-clip-text pr-2 text-xl font-extrabold text-transparent"
-              delay={100}
-              dangerouslySetInnerHTML={{ __html: title }}
-            />
-          )}
-          {copy && (
-            <AnimatedElement className="mt-[40px]" type="div" delay={200}>
-              <PortableText value={copy} />
-            </AnimatedElement>
-          )}
-          {buttonTitle && (
-            <AnimatedElement
-              className="mt-4 bg-gradient-to-tr from-[#A0C5B5] to-[#AED0D8] bg-clip-text pr-2 text-xl font-extrabold text-transparent"
-              delay={300}
-              dangerouslySetInnerHTML={{ __html: buttonTitle }}
-            />
-          )}
-          {children}
-        </div>
-        <div className={s.ImageCont}>
-          {backgroundImage ? <AnimatedBigImage image={backgroundImage} /> : null}
-        </div>
+    <section className="flex h-dvh w-full max-w-screen-3xl flex-col-reverse items-center justify-end align-middle lg:flex-row lg:justify-around lg:px-20">
+      <div
+        className={clsx(s.CopyCont, 'flex flex-col justify-center gap-3 text-center lg:text-left')}
+      >
+        {subtitle && (
+          <AnimatedElement
+            type="h3"
+            delay={0}
+            dangerouslySetInnerHTML={{ __html: subtitle }}
+            className={clsx(containsNewLine(title) && '-mt-24')}
+          />
+        )}
+        {title && (
+          <AnimatedElement
+            type="h1"
+            className="mt-8 bg-gradient-to-tr from-[#FFFFFF] to-[#97A99E] bg-clip-text pr-2 text-xl font-extrabold text-transparent"
+            delay={100}
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+        )}
+        {copy && (
+          <AnimatedElement className="mt-[40px]" type="div" delay={200}>
+            <PortableText value={copy} />
+          </AnimatedElement>
+        )}
+        {buttonTitle && (
+          <AnimatedElement
+            className="mt-4 bg-gradient-to-tr from-[#A0C5B5] to-[#AED0D8] bg-clip-text pr-2 text-xl font-extrabold text-transparent"
+            delay={300}
+            dangerouslySetInnerHTML={{ __html: buttonTitle }}
+          />
+        )}
+        {children}
       </div>
+      {backgroundImage ? <AnimatedBigImage image={backgroundImage} className="lg:w-1/2" /> : null}
     </section>
   );
 }
--- components/FlexibleContent/Modules/Roadmap/RoadmapSection.tsx
+++ components/FlexibleContent/Modules/Roadmap/RoadmapSection.tsx
@@ -8,8 +8,8 @@ import ImageBox from 'components/shared/ImageBox';
 
 import { AnimatedElement } from 'components/AnimatedComponent/AnimatedComponent';
 import NavLink from 'components/NavLink/NavLink';
-import { forwardRef, useRef } from 'react';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import { forwardRef, useRef } from 'react';
 import s from './RoadmapSection.module.sass';
 
 const circleRadius = 26;
@@ -45,177 +45,179 @@ const RoadmapSection = forwardRef(
 
     return (
       <div ref={scrollRef}>
-        <section className={s.CopyAndImage} ref={ref}>
-          <div className="relative flex flex-row">
-            <div className="ml-[-120px] mr-[60px] flex w-[120px] flex-grow flex-col items-center">
-              <figure className="progress h-full flex-grow">
-                <svg
-                  width={width}
-                  height="900px"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-full flex-grow"
-                >
-                  <line
-                    x1={x}
-                    x2={x}
-                    y1="0%"
-                    y2="100%"
-                    strokeWidth={6}
-                    strokeDasharray="9"
-                    className="stroke-[#5C5C5C]"
-                  />
-                  <motion.line
-                    x1={x}
-                    x2={x}
-                    y1={-160}
-                    y2="100%"
-                    strokeWidth={6}
-                    pathLength="1"
-                    className="stroke-[#ffffff]"
-                    style={{ pathLength: scrollYProgress }}
-                  />
-                  {isFinal ? (
-                    <>
-                      <defs>
-                        <linearGradient
-                          id="gradFinal"
-                          gradientUnits="userSpaceOnUse"
-                          x1={x}
-                          x2={x}
-                          y1={`${100 - strokeFadePercent - 20}%`}
-                          y2="100%"
-                        >
-                          <stop offset="0%" stopColor="var(--color-black)" stopOpacity={0} />
-                          <stop offset="50%" stopColor="var(--color-black)" stopOpacity={1} />
-                          <stop offset="100%" stopColor="var(--color-black)" stopOpacity={1} />
-                        </linearGradient>
-                      </defs>
-                      <line
-                        x1={x}
-                        x2={x}
-                        y1={circleY}
-                        y2="100%"
-                        strokeWidth={6}
-                        stroke="var(--color-black)"
-                      />
-                      <line
-                        x1={x}
-                        x2={x}
-                        y1={circleY}
-                        y2="100%"
-                        strokeWidth={6}
-                        strokeDasharray="9"
-                        className="stroke-[#5C5C5C]"
-                      />
-                      <line
+        <section ref={ref} className="mx-10 flex max-w-screen-3xl flex-row">
+          <div className="-ml-16 flex w-32 min-w-32 flex-col items-center lg:ml-0">
+            <figure className="progress h-full flex-grow">
+              <svg
+                width={width}
+                height="900px"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-full flex-grow"
+              >
+                <line
+                  x1={x}
+                  x2={x}
+                  y1="0%"
+                  y2="100%"
+                  strokeWidth={6}
+                  strokeDasharray="9"
+                  className="stroke-[#5C5C5C]"
+                />
+                <motion.line
+                  x1={x}
+                  x2={x}
+                  y1={-160}
+                  y2="100%"
+                  strokeWidth={6}
+                  pathLength="1"
+                  className="stroke-[#ffffff]"
+                  style={{ pathLength: scrollYProgress }}
+                />
+                {isFinal ? (
+                  <>
+                    <defs>
+                      <linearGradient
+                        id="gradFinal"
+                        gradientUnits="userSpaceOnUse"
                         x1={x}
                         x2={x}
                         y1={`${100 - strokeFadePercent - 20}%`}
                         y2="100%"
-                        strokeWidth={6}
-                        stroke="url(#gradFinal)"
-                      />
-                    </>
-                  ) : null}
-                  <motion.circle
-                    cx={x}
-                    cy={circleY}
-                    r={circleRadius}
-                    strokeWidth={3}
-                    style={{
-                      fill: 'var(--color-black)',
-                      scale: circlePassiveScale,
-                    }}
-                  />
-                  <motion.circle
-                    cx={x}
-                    cy={circleY}
-                    r={largeCircleRadius}
-                    strokeWidth={strokeWidth}
-                    className="fill-[#00F782] stroke-[#00F782]"
-                    style={{ scale: circleActiveScale }}
-                  />
-                  <motion.circle
-                    cx={x}
-                    cy={circleY}
-                    r={circleRadius}
-                    strokeWidth={3}
-                    className={'glow-large fill-[#00F782]'}
-                    style={{ scale: circleActiveScale }}
-                  />
-                  <motion.circle
-                    cx={x}
-                    cy={circleY}
-                    r={circleRadius}
-                    strokeWidth={3}
-                    style={{
-                      scale: circlePassiveScale,
-                      stroke: circleStrokeGradient,
-                      fill: circleFillGradient,
-                    }}
-                  />
-                  {isFirst ? (
-                    <>
-                      <defs>
-                        <linearGradient
-                          id="gradFirst"
-                          gradientUnits="userSpaceOnUse"
-                          x1={x}
-                          x2={x}
-                          y1="0%"
-                          y2={`${strokeFadePercent}%`}
-                        >
-                          <stop offset={`0%`} stopColor="var(--color-black)" stopOpacity={1} />
-                          <stop offset={`100%`} stopColor="var(--color-black)" stopOpacity={0} />
-                        </linearGradient>
-                      </defs>
-                      <line
+                      >
+                        <stop offset="0%" stopColor="var(--color-black)" stopOpacity={0} />
+                        <stop offset="50%" stopColor="var(--color-black)" stopOpacity={1} />
+                        <stop offset="100%" stopColor="var(--color-black)" stopOpacity={1} />
+                      </linearGradient>
+                    </defs>
+                    <line
+                      x1={x}
+                      x2={x}
+                      y1={circleY}
+                      y2="100%"
+                      strokeWidth={6}
+                      stroke="var(--color-black)"
+                    />
+                    <line
+                      x1={x}
+                      x2={x}
+                      y1={circleY}
+                      y2="100%"
+                      strokeWidth={6}
+                      strokeDasharray="9"
+                      className="stroke-[#5C5C5C]"
+                    />
+                    <line
+                      x1={x}
+                      x2={x}
+                      y1={`${100 - strokeFadePercent - 20}%`}
+                      y2="100%"
+                      strokeWidth={6}
+                      stroke="url(#gradFinal)"
+                    />
+                  </>
+                ) : null}
+                <motion.circle
+                  cx={x}
+                  cy={circleY}
+                  r={circleRadius}
+                  strokeWidth={3}
+                  style={{
+                    fill: 'var(--color-black)',
+                    scale: circlePassiveScale,
+                  }}
+                />
+                <motion.circle
+                  cx={x}
+                  cy={circleY}
+                  r={largeCircleRadius}
+                  strokeWidth={strokeWidth}
+                  className="fill-[#00F782] stroke-[#00F782]"
+                  style={{ scale: circleActiveScale }}
+                />
+                <motion.circle
+                  cx={x}
+                  cy={circleY}
+                  r={circleRadius}
+                  strokeWidth={3}
+                  className={'glow-large fill-[#00F782]'}
+                  style={{ scale: circleActiveScale }}
+                />
+                <motion.circle
+                  cx={x}
+                  cy={circleY}
+                  r={circleRadius}
+                  strokeWidth={3}
+                  style={{
+                    scale: circlePassiveScale,
+                    stroke: circleStrokeGradient,
+                    fill: circleFillGradient,
+                  }}
+                />
+                {isFirst ? (
+                  <>
+                    <defs>
+                      <linearGradient
+                        id="gradFirst"
+                        gradientUnits="userSpaceOnUse"
                         x1={x}
                         x2={x}
                         y1="0%"
                         y2={`${strokeFadePercent}%`}
-                        strokeWidth={6}
-                        stroke="url(#gradFirst)"
-                      />
-                    </>
-                  ) : null}
-                </svg>
-              </figure>
-            </div>
-            <div className={clsx(s.Cont, 'Container')}>
-              <AnimatedElement className={clsx(s.Content)} delay={100}>
-                {subtitle && (
-                  <AnimatedElement
-                    type="h3"
-                    delay={0}
-                    dangerouslySetInnerHTML={{
-                      __html: subtitle,
-                    }}
-                  />
-                )}
-                {title && (
-                  <AnimatedElement
-                    type="h2"
-                    className="mt-8 bg-gradient-to-tr from-[#FFFFFF] to-[#97A99E] bg-clip-text pr-2 text-xl font-extrabold text-transparent"
-                    delay={100}
-                    dangerouslySetInnerHTML={{ __html: title }}
-                  />
-                )}
-                {copy && (
-                  <AnimatedElement className="mb-[30px] mr-[12px] mt-[40px]" type="div" delay={200}>
-                    <PortableText value={copy} />
-                  </AnimatedElement>
-                )}
-                {showLink && link && (
-                  <NavLink {...link} className="text-[26px] font-bold text-primary">
-                    <span className={s.LinkLabel}>{link.title}</span>
-                  </NavLink>
-                )}
-              </AnimatedElement>
-              <AnimatedElement className={clsx(s.Image)} delay={200}>
-                {image && <ImageBox image={image} />}
-              </AnimatedElement>
-            </div>
+                      >
+                        <stop offset={`0%`} stopColor="var(--color-black)" stopOpacity={1} />
+                        <stop offset={`100%`} stopColor="var(--color-black)" stopOpacity={0} />
+                      </linearGradient>
+                    </defs>
+                    <line
+                      x1={x}
+                      x2={x}
+                      y1="0%"
+                      y2={`${strokeFadePercent}%`}
+                      strokeWidth={6}
+                      stroke="url(#gradFirst)"
+                    />
+                  </>
+                ) : null}
+              </svg>
+            </figure>
+          </div>
+          <div className="flex w-full flex-col-reverse items-center lg:flex-row lg:pl-24">
+            <AnimatedElement className={clsx(s.Content, 'w-full')} delay={100}>
+              {subtitle && (
+                <AnimatedElement
+                  type="h3"
+                  delay={0}
+                  dangerouslySetInnerHTML={{
+                    __html: subtitle,
+                  }}
+                />
+              )}
+              {title && (
+                <AnimatedElement
+                  type="h2"
+                  className="mt-8 bg-gradient-to-tr from-[#FFFFFF] to-[#97A99E] bg-clip-text pr-2 text-4xl font-medium text-transparent lg:text-8xl"
+                  delay={100}
+                  dangerouslySetInnerHTML={{ __html: title }}
+                />
+              )}
+              {copy && (
+                <AnimatedElement
+                  className="mb-[30px] mr-[12px] mt-[40px] text-base font-medium lg:text-3xl"
+                  type="div"
+                  delay={200}
+                >
+                  <PortableText value={copy} />
+                </AnimatedElement>
+              )}
+              {showLink && link && (
+                <NavLink {...link} className="text-[26px] font-bold text-primary">
+                  <span>{link.title}</span>
+                </NavLink>
+              )}
+            </AnimatedElement>
+            <AnimatedElement className="w-full flex-shrink-0 lg:w-1/3" delay={200}>
+              {image && <ImageBox image={image} />}
+            </AnimatedElement>
           </div>
         </section>
       </div>
