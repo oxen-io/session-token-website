0683e20059af2eda6b679f57d601732d13b48fbd
William Grant <willmgrant@gmail.com>
2024-06-14 14:16:38 +1000

    feat: migrate scroll button to tailwind

--- components/Button/ScrollButton.tsx
+++ components/Button/ScrollButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import s from './ScrollButton.module.sass';
+import clsx from 'clsx';
 
 export default function ScrollButton() {
   // When the button is clicked nudge the screen down a little
@@ -12,9 +12,15 @@ export default function ScrollButton() {
   };
 
   return (
-    <div className={s.MobileScroll} onClick={nudgeDown}>
-      <div className={s.ScrollIcon} />
-      <span>Scroll</span>
+    <div
+      className={clsx(
+        'absolute bottom-5 left-0 flex w-full flex-col items-center justify-center gap-1 opacity-70',
+        'lg:hidden'
+      )}
+      onClick={nudgeDown}
+    >
+      <div className={clsx('h-[70px] w-px bg-white opacity-70')} />
+      <span className={clsx('uppercase')}>Scroll</span>
     </div>
   );
 }
