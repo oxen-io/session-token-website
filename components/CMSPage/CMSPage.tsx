7c3f3e016c061bcfec744a9e25aaa060bc0a4c3a
William Grant <willmgrant@gmail.com>
2024-06-20 01:59:26 +1000

    fix: show 404 page in draft mode

--- components/CMSPage/CMSPage.tsx
+++ components/CMSPage/CMSPage.tsx
@@ -3,6 +3,7 @@ import FlexibleContent from '@/components/FlexibleContent/FlexibleContent';
 import PageComponents from '@/components/PageComponents/PageComponents';
 import type { PageSchemaType } from '@/schemas/documents/page';
 import type { SettingsSchemaType } from '@/schemas/singletons/settings';
+import { notFound } from 'next/navigation';
 
 export default function CMSPage({
   page,
@@ -11,6 +12,10 @@ export default function CMSPage({
   page: PageSchemaType;
   settings: SettingsSchemaType;
 }) {
+  if (!page || !page.modules) {
+    return notFound();
+  }
+
   // Default to an empty object to allow previews on non-existent documents
   const { modules } = page ?? {};
 
