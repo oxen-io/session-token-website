732f6c8d2e194e9fa2faddfef315c2b07c8584b0
Ryan Miller <ryan.m@getsession.org>
2024-06-19 12:14:53 +1000

    feat: rewrite faq list

--- schemas/objects/flexibleSections/faqsList.ts
+++ schemas/objects/flexibleSections/faqsList.ts
@@ -1,45 +1,50 @@
+import type { SchemaFields } from '@/lib/sanity.types';
 import portableText from '@/schemas/partials/portableText';
+import { baseStyles } from '@/schemas/partials/styling';
 import { defineArrayMember, defineField, defineType } from 'sanity';
+
+const faqFields = [
+  ...baseStyles,
+  defineField({
+    name: 'categories',
+    type: 'array',
+    of: [
+      defineArrayMember({
+        name: 'category',
+        type: 'object',
+        fields: [
+          defineField({
+            name: 'title',
+            type: 'string',
+          }),
+          defineField({
+            name: 'faqs',
+            type: 'array',
+            of: [
+              defineArrayMember({
+                name: 'faq',
+                type: 'object',
+                fields: [
+                  defineField({
+                    name: 'question',
+                    type: 'string',
+                  }),
+                  portableText('answer', 'Answer'),
+                ],
+              }),
+            ],
+          }),
+        ],
+      }),
+    ],
+  }),
+];
 
 export const faqsList = defineType({
   name: 'faqsList',
   type: 'object',
   title: 'FAQs List',
-  fields: [
-    defineField({
-      name: 'categories',
-      type: 'array',
-      of: [
-        defineArrayMember({
-          name: 'category',
-          type: 'object',
-          fields: [
-            defineField({
-              name: 'title',
-              type: 'string',
-            }),
-            defineField({
-              name: 'faqs',
-              type: 'array',
-              of: [
-                defineArrayMember({
-                  name: 'faq',
-                  type: 'object',
-                  fields: [
-                    defineField({
-                      name: 'question',
-                      type: 'string',
-                    }),
-                    portableText('answer', 'Answer'),
-                  ],
-                }),
-              ],
-            }),
-          ],
-        }),
-      ],
-    }),
-  ],
+  fields: faqFields,
   preview: {
     prepare() {
       return {
@@ -48,3 +53,5 @@ export const faqsList = defineType({
     },
   },
 });
+
+export type FAQSchemaType = SchemaFields<typeof faqFields>;
