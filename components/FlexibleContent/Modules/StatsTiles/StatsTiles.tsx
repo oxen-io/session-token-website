4b427dc5043975d6f089f858c9941bca18f76ccd
William Grant <willmgrant@gmail.com>
2024-06-20 16:33:43 +1000

    fix: stats tiles responsiveness

--- components/FlexibleContent/Modules/StatsTiles/StatsTiles.tsx
+++ components/FlexibleContent/Modules/StatsTiles/StatsTiles.tsx
@@ -21,8 +21,13 @@ export default function StatsTiles({
   const [attempt, setAttempt] = useState(0);
 
   return (
-    <section className={clsx('my-12')}>
-      <ul className={clsx('m-0 grid list-none grid-cols-1 gap-4 p-0', 'lg:grid-cols-3')}>
+    <section className={clsx('my-12', 'md:mb-20', 'lg:mb-12')}>
+      <ul
+        className={clsx(
+          'm-0 flex w-full list-none flex-col items-center gap-4 p-0',
+          'lg:grid lg:grid-cols-3'
+        )}
+      >
         {tiles?.map((tile, index) => {
           const { figure, copy, backgroundImage } = tile;
 
@@ -32,17 +37,27 @@ export default function StatsTiles({
               type={'li'}
               delay={index * 100 + 100}
               className={clsx(
-                'flex flex-col items-center justify-center overflow-hidden rounded-[32px] border border-solid border-[#405E56] py-6 ps-4',
-                'lg:py-8 lg:ps-10'
+                'flex flex-col items-center justify-center overflow-hidden rounded-[32px] border border-solid border-[#405E56]',
+                'h-36 w-full max-w-[350px] px-5',
+                'lg:max-w-[420px]',
+                'xl:h-48 xl:max-w-none xl:px-8'
               )}
             >
               <div
                 className={clsx(
                   'relative z-10 flex h-full w-full flex-col items-start justify-center gap-1 overflow-hidden',
-                  'lg:pt-1'
+                  'lg:pt-1',
+                  'xl:pt-0'
                 )}
               >
-                <h1 className={clsx('text-[54px] leading-none', 'lg:text-[76px]')}>
+                <h1
+                  className={clsx(
+                    'text-[54px] leading-none',
+                    'lg:text-[44px]',
+                    'xl:text-[68px]',
+                    '2xl:text-[72px]'
+                  )}
+                >
                   <NumberTicker
                     key={`${id}-attempt-${attempt}`}
                     targetNumber={parseFloat(figure)}
@@ -52,7 +67,10 @@ export default function StatsTiles({
                 <p className={clsx('w-full text-2xl leading-none')}>{copy}</p>
               </div>
               {backgroundImage && (
-                <ImageBox image={backgroundImage} className={clsx('absolute left-0 top-0')} />
+                <ImageBox
+                  image={backgroundImage}
+                  className={clsx('absolute left-0 top-0 *:h-36', 'xl:*:h-48')}
+                />
               )}
             </AnimatedElement>
           );
