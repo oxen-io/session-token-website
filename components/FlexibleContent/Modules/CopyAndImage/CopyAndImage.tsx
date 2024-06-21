6c21241734a6b89cf00664f459d8e2ee056c2a69
William Grant <willmgrant@gmail.com>
2024-06-21 12:03:12 +1000

    fix: increased copyandimage padding

--- components/FlexibleContent/Modules/CopyAndImage/CopyAndImage.tsx
+++ components/FlexibleContent/Modules/CopyAndImage/CopyAndImage.tsx
@@ -34,7 +34,7 @@ export default function CopyAndImage(props: CopyAndImageProps) {
   }
 
   return (
-    <section className={clsx('my-12', 'lg:my-24')}>
+    <section className={clsx('my-12', 'md:my-24', 'lg:my-32')}>
       <div
         className={clsx(
           'flex items-center justify-between',
