c62dd1f4973f1c51ca2daf22f8f4fbfa53c64517
Ryan Miller <ryan.m@getsession.org>
2024-06-21 10:17:01 +1000

    feat: create background gradient component

--- components/FlexibleContent/Modules/TileGroup/TileGroup.tsx
+++ components/FlexibleContent/Modules/TileGroup/TileGroup.tsx
@@ -1,21 +1,21 @@
+import BackgroundGradientContainer from '@/components/BackgroundGradientContainer/BackgroundGradientContainer';
 import type { TileGroupSchemaType } from '@/schemas/objects/flexibleSections/tileGroup';
 import Tile from '../Tile/Tile';
 
 export default function TileGroup({ tiles, title, showPrimaryBackground }: TileGroupSchemaType) {
+  const Comp = showPrimaryBackground ? BackgroundGradientContainer : 'section';
   return (
-    <section className="relative flex w-full flex-col items-center justify-center gap-14 px-2 lg:px-0">
-      {showPrimaryBackground ? (
-        <>
-          <div className="absolute hidden h-full w-screen bg-gradient-to-b from-transparent via-primary to-transparent opacity-10 lg:block" />
-          <div className="absolute hidden h-full w-screen bg-gradient-to-r from-[#0a0a0a] via-transparent to-[#0a0a0a] opacity-90 lg:block" />
-        </>
-      ) : null}
+    <Comp
+      as="section"
+      className="relative flex w-full flex-col items-center justify-center gap-14 px-2 lg:px-0"
+      {...(showPrimaryBackground && { gradientClassName: 'w-screen' })}
+    >
       {title && <h1 className="hidden text-center text-5xl font-medium md:block">{title}</h1>}
       <ul className="flex w-full flex-col items-start justify-center gap-10 lg:flex-row lg:gap-24">
         {tiles?.map((tile, i) => {
           return <Tile key={i} {...tile} />;
         })}
       </ul>
-    </section>
+    </Comp>
   );
 }
